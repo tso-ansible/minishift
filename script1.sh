d932b7bc93dbd3a8d25c4eb3778dd7126fd14268
venerari <35533668+venerari@users.noreply.github.com>
2018-02-02 16:52:52 -0500

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -15,7 +15,7 @@ yum install gcc make kernel-devel -y
 update-rc.d script2.sh defaults
 
 echo "This will reboot now, unfortunately after reboot, it should continue to install on the background."
-echo "Check the /tmp/minishitisdone file.  If it does not exist, it is not done, you may also check"
+echo "Check the /tmp/minishitisdone file existence.  If it does not exist, it is not done, you may also check"
 echo "the second script on the process ps -ef script2.sh"
 
 sleep 10
