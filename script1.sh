fbc966f79019107cb88ac8c78840216394becece
venerari <35533668+venerari@users.noreply.github.com>
2018-02-02 10:30:54 -0500

    Create script1.sh

---
+++ script1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+curl -o  /etc/yum.repos.d/centos.repo https://raw.githubusercontent.com/tso-ansible/ansible-tower/master/centos.repo
+
+yum clean all
+
+yum install epel-release -y
+
+yum update -y
+
+yum install git ansible wget -y
+
+if [ ! -f ~/.ssh/id_rsa ]; then
+	ssh-keygen -q -f ~/.ssh/id_rsa -N ""
+	cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+	ssh -o StrictHostKeyChecking=no localhost "pwd" < /dev/null
+fi
+
+cd /tmp
